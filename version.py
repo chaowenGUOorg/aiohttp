863cf319e01b0ffcf21b07f14fca2c6ca6fccc47
chaowen guo <chaowen.guo1@gmail.com>
2021-01-10 22:31:20 -0600

    Update version.py

--- version.py
+++ version.py
@@ -5,7 +5,7 @@ async def f():
     container = await docker.containers.create({'Cmd':['python', '-c', 'import platform; print(platform.python_version()[:3])'], 'Image':'python:slim'})
     await container.start()
     pathlib.Path(os.getenv('GITHUB_ENV')).write_text('VERSION=' + ''.join(await container.log(stdout=True)))
-    pathlib.Path(os.getenv('GITHUB_ENV')).read_text()
+    print(pathlib.Path(os.getenv('GITHUB_ENV')).read_text(),flush=True)
     await container.delete(force=True)
     await docker.close()
     
