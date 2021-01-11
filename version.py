a64be1d8656bea8e152d2fddee77518de5476b6a
chaowen guo <chaowen.guo1@gmail.com>
2021-01-10 22:25:59 -0600

    Update version.py

--- version.py
+++ version.py
@@ -2,7 +2,7 @@ import asyncio, pathlib, os, aiodocker
 async def f():
     docker = aiodocker.Docker()
     await docker.images.pull('python', tag='slim')
-    container = await docker.containers.create(config={'Cmd':['python', '-c', 'import platform; print(platform.python_version()[:3])'], 'Image':'python:slim'})
+    container = await docker.containers.create({'Cmd':['python', '-c', 'import platform; print(platform.python_version()[:3])'], 'Image':'python:slim'})
     await container.start()
     pathlib.Path(os.getenv('GITHUB_ENV')).write_text('VERSION=' + ''.join(await container.log(stdout=True)))
     await container.delete(force=True)
